6cdd1ff93279ee9ce8a88917722a67dd692637bb
Richard <therichkid@gmx.de>
2024-11-04 21:12:48 +0100

    Remove event calendar, setup own implementation

---
+++ src/lib/components/Calendar/Calendar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { CalendarView } from '$lib/models/calendar';
+	import MonthView from './MonthView.svelte';
+
+	let { view }: { view: CalendarView } = $props();
+</script>
+
+{#if view === 'month'}
+	<MonthView />
+{/if}
--- src/routes/admin/events/+page.svelte
+++ src/routes/admin/events/+page.svelte
@@ -1,24 +1,6 @@
 <script lang="ts">
-	import Calendar from '@event-calendar/core';
-	import DayGrid from '@event-calendar/day-grid';
-	import Interaction from '@event-calendar/interaction';
-	import List from '@event-calendar/list';
-	import ResourceTimeline from '@event-calendar/resource-timeline';
-	import TimeGrid from '@event-calendar/time-grid';
-
-	const plugins = [DayGrid, TimeGrid, List, ResourceTimeline, Interaction];
-	const options: Calendar.Options = {
-		events: [],
-		firstDay: 1,
-		headerToolbar: {
-			start: 'prev,next today',
-			center: 'title',
-			end: 'dayGridMonth,timeGridWeek,listMonth,resourceTimelineWeek,resourceTimelineMonth',
-		},
-		slotMinTime: '06:00:00',
-		slotMaxTime: '22:00:00',
-		view: 'dayGridMonth',
-	};
+	import Calendar from '$components/Calendar/Calendar.svelte';
+	import { CalendarView } from '$lib/models/calendar';
 </script>
 
-<Calendar {plugins} {options} />
+<Calendar view={CalendarView.Month} />
